122994d7cac287c024292416ea97bb6dea9de8db
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-07-18 20:37:58 +0200

    Fix: Bad copy/paste

--- src/main/java/fr/pederobien/vocal/server/event/VocalClientDisconnectPostEvent.java
+++ src/main/java/fr/pederobien/vocal/server/event/VocalClientDisconnectPostEvent.java
@@ -19,7 +19,7 @@ public class VocalClientDisconnectPostEvent extends PlayerVocalClientEvent {
 	public String toString() {
 		StringJoiner joiner = new StringJoiner(", ", "{", "}");
 		joiner.add("server=" + getClient().getServer().getName());
-		joiner.add("client=#%s" + getClient().hashCode());
+		joiner.add("client=#" + getClient().hashCode());
 		return String.format("%s_%s", getName(), joiner);
 	}
 }
